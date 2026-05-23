df8269738a0054c781f7bd92f460edef735f7d32
baojie <baojie@gmail.com>
2026-05-23 22:25:17 +0800

    -m

--- local/config/home.js
+++ local/config/home.js
@@ -1,7 +1,7 @@
 // WIKI_LANG = zh
 export const CORE_FEATURED = [];
-export const PREFACE_IDS   = [];
-export const APPENDIX_IDS  = [];
+export const PREFACE_IDS   = ['目录', 'Preface'];
+export const APPENDIX_IDS  = ['Epilogue'];
 export const HOME_SECTIONS = [
   { label: '核心概念', tag: null, type: 'concept', featuredOnly: false, limit: 8 },
   { label: '重要人物', tag: null, type: 'person',  featuredOnly: false, limit: 6 },
